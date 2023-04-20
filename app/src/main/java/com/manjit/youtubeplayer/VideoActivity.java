09b696d2eb66e3a81f9754318b600f0170c737ef
chhotuthakur <npsdahiya@gmail.com>
2023-04-20 17:49:19 +0530

    Initial commit

---
+++ app/src/main/java/com/manjit/youtubeplayer/VideoActivity.java
@@ -0,0 +1,32 @@
+package com.manjit.youtubeplayer;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
+
+public class VideoActivity extends AppCompatActivity {
+
+    public String Vid;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video);
+
+        YouTubePlayerView youTubePlayerView = findViewById(R.id.youtube_player_view);
+        getLifecycle().addObserver(youTubePlayerView);
+
+        youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+            @Override
+            public void onReady(@NonNull YouTubePlayer youTubePlayer) {
+                String videoId = "66rknFPqZbs";
+                youTubePlayer.loadVideo(videoId, 0);
+            }
+        });
+    }
+}
